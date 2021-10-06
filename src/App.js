2d7804f1cb58fa094641de041d729753948f8308
unknown <ImSparrowteal@gmail.com>
2021-10-06 13:45:03 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+const key = "9b5bc6dd74eb4b7fb66e2eb7031b13f1";
+const App = () => {
+  const cityRef = useRef();
+  const [weather, setWeather] = useState();
+  const [loading, setLoading] = useState(false);
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    const city = cityRef.current.value;
+    const { lat, lng } = await getLonLat(city);
+    const data = await searchCity(lat, lng);
+    setWeather(data);
+    setLoading(false);
+  };
 
-function App() {
+  const getLonLat = async (city) => {
+    try {
+      const result = await fetch(
+        `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=${key}`
+      );
+      if (!result.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await result.json();
+      const lng = data.results[0].bounds.northeast.lng;
+      const lat = data.results[0].bounds.northeast.lat;
+      return { lat, lng };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const searchCity = async (lat, lng) => {
+    try {
+      const result = await fetch(
+        `http://www.7timer.info/bin/api.pl?lon=${lng}&lat=${lat}&product=civil&output=json`
+      );
+      if (!result.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await result.json();
+      return data.dataseries.slice(0, 7);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form id="form">
+        <label htmlFor="city">City Name:</label>
+        <input type="text" id="city" name="city" ref={cityRef}></input>
+        <button type="submit" onClick={submitHandler}>
+          Search!
+        </button>
+      </form>
+      {loading && <div>Loading...</div>}
+      {weather && (
+        <div>
+          {weather.map((forecast) => (
+            <WeatherComponent
+              key={Math.random()}
+              weather={forecast.weather}
+            ></WeatherComponent>
+          ))}
+        </div>
+      )}
+    </>
   );
-}
+};
+
+const WeatherComponent = (props) => {
+  return <div>{props.weather}</div>;
+};
 
 export default App;
